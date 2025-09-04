4ac9bcbcd4783ce4da41f52914606c20969b32d3
slim <slimabida21@gmail.com>
2025-09-04 10:46:22 +0200

    création de serverActions pour l'envoie des articles sur la base de donée avec le postServerActions.js

---
+++ src/lib/ServerActions/blog/postServerActions.js
@@ -0,0 +1,23 @@
+import { connectToDB } from "@/lib/utils/db/connectToDB"
+import { connection } from "mongoose"
+
+
+export async function addPost(formData) {
+  const { title, markdownArticle} = Object.fromEntries(formData)
+
+  try {
+    await connectToDB()
+    const newPost = new Post({
+      title, 
+      markdownArticle
+    })
+    const savedPost = await newPost.save()
+    console.log("Post saved.")
+    return {success: true, slug: savedPost.slug}
+    
+  } catch (error) {
+      console.log("error while creating the post :", error)
+      throw new Error(error.message || "An error occured while creating the post");
+      
+  }
+}
