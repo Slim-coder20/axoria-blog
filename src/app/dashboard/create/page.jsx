7d898a1f1e02de7eaaa14a6c92d7501b7d25f6dd
slim <slimabida21@gmail.com>
2025-09-04 14:24:11 +0200

    Mise en place de l'input pour ajouter des tags dans le formulaire de la création d'articles

--- src/app/dashboard/create/page.jsx
+++ src/app/dashboard/create/page.jsx
@@ -1,15 +1,27 @@
 "use client";
 import { addPost } from "@/lib/ServerActions/blog/postServerActions";
+import { useState, useRef } from "react";
 
 export default function page() {
+  // Instanciaotion du state //
+  const [tags, setTags] = useState(["css", "javaScript"]);
+
+  // Instanciation du UseRef //
+  const tagInputRef = useRef(null);
+
+  // Création de la fonction HandleAddTag //
+  function handleAddTag() {}
+
+  function handleRrmoveTag() {}
+
   async function handleSubmit(e) {
     e.preventDefault();
 
-    const formData = new FormData(e.target)
-    for(const [key, value] of formData.entries()){
-      console.log(key, value); 
+    const formData = new FormData(e.target);
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value);
     }
-    const result = await addPost(formData)
+    const result = await addPost(formData);
   }
   return (
     <main className="u-main-container bg-white p-7 mt-32 mb-44">
@@ -26,6 +38,44 @@ export default function page() {
           placeholder="Tilte"
           required
         />
+        <div className="mb-10">
+          <label className="f-label" htmlFor="tag">
+            Add a tag(s) (optional, max5)
+          </label>
+          <div className="flex">
+            <input
+              type="text"
+              className="shadow border rounded p-2 text-gray-700 focus:outline-slate-400"
+              id="tag"
+              placeholder="Add a tag"
+              ref={tagInputRef}
+            />
+            <button
+              className="bg-indigo-500 hover:bg-indigo-700 p-4 rounded mx-4 text-white font-bold "
+              onClick={handleAddTag}
+              type="button"
+            >
+              Add
+            </button>
+            <div className="flex items-center grow whitespace-nowrap overflow-y-auto shadow border rounded px-3">
+              {tags.map((tag) => (
+                <span
+                  key={tag}
+                  className="inline-block whitespace-nowrap bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2"
+                >
+                  {tag}
+                  <button
+                    type="button"
+                    onClick={ () => handleRemoveTag(tag)}
+                    className=" text-red-500 ml-2"
+                  >
+                     &times; 
+                  </button>
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
         <label htmlFor="markdownArticle" className="f-label">
           Write your article using markdown - do not repeat the already given
           title
@@ -44,12 +94,13 @@ export default function page() {
           className=" min-h-44 text-xl shadow 
           appearance-none border rounded w-full p-8 text-gray-700 mb-4 focus:outline-slate-400 "
         ></textarea>
-        <button className="min-w-44 bg-indigo-500 py-3 px-4 rounded text-white hover:bg-indigo-700 font-bold border-none mb-4">Submit</button>
+        <button className="min-w-44 bg-indigo-500 py-3 px-4 rounded text-white hover:bg-indigo-700 font-bold border-none mb-4">
+          Submit
+        </button>
       </form>
     </main>
   );
 }
 
-
-// slimabida21_db_user 
+// slimabida21_db_user
 // abi123
